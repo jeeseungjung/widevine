60478188b8cb9021df89898071aa594d552dcea6
jeeseung <jeeseung.jung@lge.com>
2022-07-14 15:58:50 +0900

    Update widevine test

---
+++ js/reading-csv-file-using-javascript.js
@@ -0,0 +1,68 @@
+function buildTable(results){
+
+	var markup = "<table class='table'>";
+	var data = results.data;
+
+	for(i=0;i<data.length;i++){
+		markup+= "<tr>";
+		var row = data[i];
+		var cells = row.join(";").split(";");
+
+		for(j=0;j<cells.length;j++){
+			markup+= "<td>";
+			console.log(cells[j]);
+			markup+= cells[j];
+			markup+= "</th>";
+
+		}
+		markup+= "</tr>";
+	}
+	markup+= "</table>";
+
+
+	$("#app").html(markup);
+}
+
+
+$(document).ready(function(){
+		$('#submit2').on("click",function(e){
+			// e.preventDefault();
+			// if (!$('#files')[0].files.length){
+			// 	alert("Please choose at least one file to read the data.");
+			// }
+
+			// $('#files').parse({
+			// file = "dash.csv";
+			// Papa.parse(file, {
+			// 	config: {
+			// 		download: true,
+			// 		delimiter: ";",
+			// 		complete: buildTable,
+			// 		//header: true,
+			// 	},
+			// 	before: function(file, inputElem)
+			// 	{
+			// 		//console.log("Parsing file...", file);
+			// 	},
+			// 	error: function(err, file)
+			// 	{
+			// 		console.log("ERROR:", err, file);
+			// 	},
+			// 	complete: function()
+			// 	{
+			// 		//console.log("Done with all files");
+			// 	}
+			// });
+			// var config = {
+			// 	download: true,
+			// 	delimiter: ";",
+			// 	complete: buildTable,
+			// };
+			file = "./dash.csv";
+			Papa.parse(file, {
+				download: true,
+				delimiter: ";",
+				complete: buildTable,
+			});
+		});
+});
